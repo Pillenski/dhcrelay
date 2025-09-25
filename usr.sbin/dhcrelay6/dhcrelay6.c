a41f855e2800cefe7acd3241bd715e5cb87322ec
Sebastian Reiche <s.reiche@lebenshilfe-dresden.de>
2025-09-25 14:12:35 +0200

    only forward RELAY-REPLY from server-side interface to client-side interface

--- usr.sbin/dhcrelay6/dhcrelay6.c
+++ usr.sbin/dhcrelay6/dhcrelay6.c
@@ -765,16 +765,23 @@ void
 relay6_logsrcaddr(struct packet_ctx *pc, struct interface_info *intf,
     uint8_t msgtype)
 {
+	relay6_logsrcaddraction(pc, intf, msgtype, "forwarded");
+}
+
+void
+relay6_logsrcaddraction(struct packet_ctx *pc, struct interface_info *intf,
+    uint8_t msgtype, const char *action)
+{
 	const char		*type;
 
-	type = (msgtype == DHCP6_MT_RELAYREPL) ? "reply" : "forward";
+	type = dhcp6type2str(msgtype);
 	if (drm == DRM_LAYER2)
-		log_info("forwarded relay-%s for %s to %s",
-		    type, print_hw_addr(pc->pc_htype, pc->pc_hlen,
+		log_info("%s %s for %s to %s",
+		    action, type, print_hw_addr(pc->pc_htype, pc->pc_hlen,
 		    pc->pc_smac), intf->name);
 	else
-		log_info("forwarded relay-%s for %s to %s%%%s",
-		    type,
+		log_info("%s %s for %s to %s%%%s",
+		    action, type,
 		    v6addr2str(&ss2sin6(&pc->pc_srcorig)->sin6_addr),
 		    v6addr2str(&ss2sin6(&pc->pc_dst)->sin6_addr),
 		    intf->name);
@@ -911,10 +918,14 @@ relay6(struct interface_info *intf, void *p, size_t plen,
 		return;
 	}
 
-	/* Or send packet to the client. */
+	/* Or send packet to the client. But only if it is a relay reply message. */
 	if (clientdir) {
-		relay6_logsrcaddr(pc, interfaces, msgtype);
-		send_packet(interfaces, p, buflen, pc);
+		if (msgtype == DHCP6_MT_RELAYREPL) {
+			relay6_logsrcaddr(pc, interfaces, msgtype);
+			send_packet(interfaces, p, buflen, pc);
+		} else {
+			relay6_logsrcaddraction(pc, interfaces, msgtype, "dropped");
+		}
 		return;
 	}
 
